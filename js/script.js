4e8db2ca1df491accb2e2ae982a45a3f9a46cd48
Hillel1408 <etemax@bk.ru>
2023-04-06 18:41:29 +0300

    jetty-cafe

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    /*autoplay: {
+        delay: 2000,
+    },
+    loop: true,*/
+});
